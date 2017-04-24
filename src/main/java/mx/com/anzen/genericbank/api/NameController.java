ab4e762e89896e1eb50b81d05bfee37ef9613892
reynaldo-ivan <mlivan@live.com>
2017-04-24 12:21:55 -0500

    Update NameController.java

--- src/main/java/mx/com/anzen/genericbank/api/NameController.java
+++ src/main/java/mx/com/anzen/genericbank/api/NameController.java
@@ -1,18 +1,14 @@
 package mx.com.anzen.genericbank.api;
 
+import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import com.mongodb.util.JSON;
-
+import org.springframework.web.bind.annotation.RestController; 
 import mx.com.anzen.mongo.services.MongoService;
 
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
    
@@ -21,15 +17,31 @@ public class NameController {
 	
 	@Autowired
 	private MongoService mongo;
-	 
-	@CrossOrigin(origins = "http://54.215.247.221:9092")
-	@RequestMapping(value="/layout")
-    public Map<String,Object> layout(@RequestBody JSONObject  json ) throws UnknownHostException{ 
+	
+
+private static Logger log = Logger.getLogger(NameController.class);
+	   
+	@RequestMapping(value="/Layout")
+    public Map<String,Object> layout(@RequestBody JSONObject  json ) { 
 		
 		Map<String,Object> map=new HashMap();
-		map.put("fileDefinition.idFileType",json.get("IdFileType").toString()); 
-		Map<String,Object> mapResult=mongo.consulta("ADBancaGenerica",map); 
-		  
+		Map<String,Object> mapResult=null;
+		try{
+			map.put("fileDefinition.idFileType",json.get("IdFileType")); 
+			mapResult=mongo.consulta("ADBancaGenerica",map); 
+			  
+		}catch (HttpMessageNotReadableException e) {
+			System.out.println("exception");
+			log.info("Error: "+e.getMessage());
+			mapResult.put("CodigoError: ",100);
+			mapResult.put("Error: ",e.getMessage()); 
+			 
+		}catch (Exception e) {
+			log.info("Error: "+e.getMessage());
+			mapResult.put("CodigoError: ",100);
+			mapResult.put("Error: ",e.getMessage()); 
+		}
+		
         return mapResult;
     }
 }
